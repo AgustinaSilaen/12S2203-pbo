768d02629a1f183cff82db6e702c7571f9d5f413
GadingOMG <gadingthebeatboxer@gmail.com>
2020-02-05 18:25:25 +0700

    Create directory structure

---
+++ work/assignments/01-24-12S18023-12S1855/Language-Fundamental/Utama.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Utama {
+/* program emtry point */ 
+public static void Utama(String[] _args) { 
+System.out.println("Hello, world!"); 
+    }
+}
---
+++ work/assignments/01-24-12S18023-12S1855/Language-Fundamental/newClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Agustina Silaen 12S18023 & Raja Muda Gading Tulen Sihite 12S18055
+ */
+public class newClass {
+/* program emtry point */ 
+public static void newClass(String[] _args) {
+ System.out.println("Hello, world!");
+ }
+}
